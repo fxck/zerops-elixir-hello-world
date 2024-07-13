a952ace17ce6f7b818dc110e61ec884ef878cae2
Aleš <foxxcz@gmail.com>
2024-07-13 13:50:24 +0200

    Update application.ex

--- lib/app/application.ex
+++ lib/app/application.ex
@@ -2,8 +2,10 @@ defmodule App.Application do
   use Application
 
   def start(_type, _args) do
+    port = String.to_integer(System.get_env("PORT") || "4000")
+
     children = [
-      {Plug.Cowboy, scheme: :http, plug: App.Router, options: [port: 4000]}
+      {Plug.Cowboy, scheme: :http, plug: App.Router, options: [port: port, ip: {0,0,0,0}]}
     ]
 
     opts = [strategy: :one_for_one, name: App.Supervisor]
